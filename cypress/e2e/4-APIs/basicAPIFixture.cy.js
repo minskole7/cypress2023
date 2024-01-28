7d85ff831d5fe715245577459ad948d7ef512f04
minskole7 <minskole7@gmail.com>
2024-01-28 13:41:44 +0530

    addded

---
+++ cypress/e2e/4-APIs/basicAPIFixture.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+
+describe('verify the POST , PUT call', function () {
+
+    it('verify the POST request', function () {
+
+        cy.fixture('payload').then(function(el){
+            cy.request({
+                url: "https://reqres.in/api/users",
+                method: "POST",
+                body: el
+            })
+            .then(function(response){
+                cy.log(response)
+            })
+
+        })
+            
+
+       
+
+
+
+    })
+
+
+})
---
+++ cypress/fixtures/contact4.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        firstName: "chinmay",
+        lastName: "deshpande",
+        email:"chinmay.deshpadne010@gmail.com",
+        message:"i am learning js from fixture folder"
+    },
+    {
+        firstName: "chinmay2",
+        lastName: "deshpande2",
+        email:"chinmay.deshpadne0102@gmail.com",
+        message:"i am learning js from fixture folder2"
+    },
+    {
+        firstName: "chinmay3",
+        lastName: "deshpande3",
+        email:"chinmay.deshpadne0103@gmail.com",
+        message:"i am learning js from fixture folder2"
+    }
+
+]
